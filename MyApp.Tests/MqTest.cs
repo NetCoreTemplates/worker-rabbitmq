7a1ab13c7782452c1e0b26e21f32434297c44515
Demis Bellot <demis.bellot@gmail.com>
2021-11-12 02:23:12 +0800

    Upgrade to .NET 6.0

--- MyApp.Tests/MqTest.cs
+++ MyApp.Tests/MqTest.cs
@@ -4,32 +4,31 @@ using NUnit.Framework;
 using ServiceStack.Messaging;
 using ServiceStack.RabbitMq;
 
-namespace MyApp.Tests
+namespace MyApp.Tests;
+
+public class MqTest
 {
-    public class MqTest
+    private readonly RabbitMqMessageFactory MqFactory;
+    public MqTest()
     {
-        private readonly RabbitMqMessageFactory MqFactory;
-        public MqTest()
-        {
-            MqFactory = new RabbitMqMessageFactory("localhost:5672");
-        }
+        MqFactory = new RabbitMqMessageFactory("localhost:5672");
+    }
 
-        [Test] // requires running Host MQ Server project
-        public void Can_send_Request_Reply_message()
+    [Test] // requires running Host MQ Server project
+    public void Can_send_Request_Reply_message()
+    {
+        using (var mqClient = MqFactory.CreateMessageQueueClient())
         {
-            using (var mqClient = MqFactory.CreateMessageQueueClient())
+            var replyToMq = mqClient.GetTempQueueName();
+
+            mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" })
             {
-                var replyToMq = mqClient.GetTempQueueName();
+                ReplyTo = replyToMq,
+            });
 
-                mqClient.Publish(new Message<Hello>(new Hello { Name = "MQ Worker" })
-                {
-                    ReplyTo = replyToMq,
-                });
-
-                var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
-                mqClient.Ack(responseMsg);
-                Assert.That(responseMsg.GetBody().Result, Is.EqualTo("Hello, MQ Worker!"));
-            }
+            var responseMsg = mqClient.Get<HelloResponse>(replyToMq);
+            mqClient.Ack(responseMsg);
+            Assert.That(responseMsg.GetBody().Result, Is.EqualTo("Hello, MQ Worker!"));
         }
     }
 }
